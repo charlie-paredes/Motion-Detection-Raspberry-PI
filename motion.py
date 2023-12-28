6e23c5d373df86ab39117ff02112dd66a4c789c7
Carlos Paredes <96803913+charlie-paredes@users.noreply.github.com>
2023-12-28 11:36:55 -0800

    Update motion.py

--- motion.py
+++ motion.py
@@ -10,9 +10,9 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 #Email configuration
-sender_email = ''
-sender_password = ''
-receiver_email = ''
+sender_email = 'charlie.paredes619@gmail.com'
+sender_password = 'asca vdxs vsgr txcj'
+receiver_email = 'cparedes2696@sdsu.edu'
 subject = 'Motion Detected!'
 body = 'Motion has been detected in the surveillance area.'
 
@@ -30,7 +30,6 @@ start_time = time.time()
 cooldown_time = 10  # seconds
 last_email_time = 0
 motion_detected = False
-output_file1 = None
 
 if not motion_detected:
     # Start capturing video using 'ffmpeg'
@@ -72,53 +71,50 @@ while True:
             print('**************************************************************')
             print("Motion detected!")
             print('**************************************************************')
-
-            # Create an output file if motion is detected
-            if output_file1 is None:
-                # Send email notification after creating the output file
-                if (time.time() is not None) or last_email_time == 0:
-                    video_capture_process.kill()
+            
+            # Send email notification after creating the output file
+            if (time.time() is not None) or last_email_time == 0:
+                video_capture_process.kill()
                     
-                    video_file = "captured_video.mp4"
+                video_file = "captured_video.mp4"
 
                     # Capture video using the function
-                    capture_video(video_file)
+                capture_video(video_file)
                     
-                    smtp_server = "smtp.gmail.com"
-                    smtp_port = 587
+                smtp_server = "smtp.gmail.com"
+                smtp_port = 587
 
-					#Create Mime object with necessary Info
-                    message = MIMEMultipart()
-                    message['From'] = sender_email
-                    message['To'] = receiver_email
-                    message['Subject'] = subject
-                    message.attach(MIMEText(body, 'plain'))
-					
-					#attach captured video to the email generated
-                    attachment = open(video_file, "rb")
-                    part = MIMEBase('application', 'octet-stream')
-                    part.set_payload((attachment).read())
-                    encoders.encode_base64(part)
-                    part.add_header('Content-Disposition', f"attachment; filename= {video_file}")
-                    message.attach(part)
+				#Create Mime object with necessary Info
+                message = MIMEMultipart()
+                message['From'] = sender_email
+                message['To'] = receiver_email
+                message['Subject'] = subject
+                message.attach(MIMEText(body, 'plain'))					
+				#attach captured video to the email generated
+                attachment = open(video_file, "rb")
+                part = MIMEBase('application', 'octet-stream')
+                part.set_payload((attachment).read())
+                encoders.encode_base64(part)
+                part.add_header('Content-Disposition', f"attachment; filename= {video_file}")
+                message.attach(part)
                     
-                    #Connect to gmail server
-                    with smtplib.SMTP(smtp_server, smtp_port) as server:
-                        server.starttls()
-                        server.login(sender_email, sender_password)
-                        server.sendmail(sender_email, receiver_email, message.as_string())
-                    video_capture_process = subprocess.Popen([
-					"ffmpeg",
-					"-f", "v4l2",
-					"-framerate", "30",
-					"-video_size", "640x480",
-					"-i", "/dev/video1",
-					"-f", "rawvideo",
-					"-"
-				    ], stdout=subprocess.PIPE)
-                    print('*****************************************************')
-                    print('Email sent successfully!')
-                    print('**************************************************************')
+                #Connect to gmail server
+                with smtplib.SMTP(smtp_server, smtp_port) as server:
+                    server.starttls()
+                    server.login(sender_email, sender_password)
+                    server.sendmail(sender_email, receiver_email, message.as_string())
+                video_capture_process = subprocess.Popen([
+				"ffmpeg",
+				"-f", "v4l2",
+				"-framerate", "30",
+				"-video_size", "640x480",
+				"-i", "/dev/video1",
+				"-f", "rawvideo",
+				"-"
+				], stdout=subprocess.PIPE)
+                print('*****************************************************')
+                print('Email sent successfully!')
+                print('**************************************************************')
 				    
 			#capture current time and update last email time
             motion_time = time.time()
